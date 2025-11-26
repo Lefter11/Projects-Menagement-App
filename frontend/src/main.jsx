1316a96a55a2b11281ad584fc817a0c2d1e78f13
Lefter11 <lefter.xhoka3@gmail.com>
2025-11-26 13:13:55 +0100

    Configure db connection and Render backend deploy

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import { Toaster } from 'react-hot-toast';
+
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -14,7 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   }}
 >
       <App />
-      <Toaster position="top-right" />
+   
     </BrowserRouter>
   </React.StrictMode>
 );
